d77938b93b2f8be82f54e820567d6b354db7e20a
Alex H <alex.holder@bigfootds.com>
2023-05-18 21:16:59 +1000

    fix: NoteDisplay component renders finds matching data now

--- src/components/NoteDisplay.jsx
+++ src/components/NoteDisplay.jsx
@@ -1,25 +1,18 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useState } from "react";
 import { useNoteData } from "../contexts/NotesContext";
 
 export default function NoteDisplay(props){
 
 	const {id} = props;
-	let note = useRef({});
+	let [localNote, setLocalNote] = useState({});
 
 	const globalNotesData = useNoteData();
-
-
 
 	useEffect(() => {
 		// On start, find the note in globalNotesData
 		// that has an ID matching props.id
-		note.current = globalNotesData.filter(globalSpecificNote => {
-			return globalSpecificNote.id === id;
-		})
-	// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [])
-
-	
+		setLocalNote(globalNotesData.find(globalSpecificNote => globalSpecificNote.id === id));
+	}, [globalNotesData, id])
 
 	return(
 		<div>
@@ -31,13 +24,13 @@ export default function NoteDisplay(props){
 					- due date (JS Date)
 					- created at date (JS Date) 
 					*/}
-					<h4>{note.current.title}</h4>
-					<p>{note.current.description}</p>
-					<p>{note.current.isCompleted ? "COMPLETE" : "NOT YET DONE"}</p>
-					<input type="checkbox" disabled="disabled" onChange={null} readOnly={true} value={note.current.isCompleted} />
-					<h5>Due Date: {new Date(note.current.dueDate).toLocaleDateString()}</h5>
+					<h4>{localNote.title}</h4>
+					<p>{localNote.description}</p>
+					<p>{localNote.isCompleted ? "COMPLETE" : "NOT YET DONE"}</p>
+					<input type="checkbox" disabled="disabled" onChange={null} readOnly={true} value={localNote.isCompleted} />
+					<h5>Due Date: {new Date(localNote.dueDate).toLocaleDateString()}</h5>
 					{/* <input type="date" readOnly value={note.dueDate} /> */}
-					<h5>Created At: {new Date(note.current.createdAtDate).toLocaleDateString()}</h5>
+					<h5>Created At: {new Date(localNote.createdAtDate).toLocaleDateString()}</h5>
 					{/* <input type="datetime-local" readOnly value={note.createdAtDate} /> */}
 		</div>
 	)
