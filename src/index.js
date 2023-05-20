dd1602f30c82e175a0556b4f4acb95847e9da459
Alex H <alex.holder@bigfootds.com>
2023-05-20 14:28:43 +1000

    feat: add custom bootstrap theme settings

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
+import './theme_1684548322906.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
